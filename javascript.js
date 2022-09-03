ca97d4b7fb992d2c512cf6fcdc7c711ece477441
Monito <phil.menard1668@gmail.com>
2022-09-02 21:51:18 -0300

    Update the functions on the calculator. Now can operate once with any operator.

--- javascript.js
+++ javascript.js
@@ -17,58 +17,71 @@ buttonsArray.forEach(button => addEventListener('mousedown', displayText));
 // fonction pour afficher les chiffres sur l'écran
 function displayText(e)  {
 
-    clearScreen(e);
-    addNumber(e);
-    setOperator(e);
-    SetOperation(e);
+    if (chiffres.includes(e.target.textContent))   {   // si le bouton cliqué est un chiffre
+        addNumber(e);
+    }
+    else if (e.target.textContent == clearButton.textContent)    {  // si le bouton cliqué est le bouton 'clear'
+        clearScreen();
+    }
+    else if (operators.includes(e.target.textContent))  {   // si le bouton cliqué est un opérateur
+        setOperator(e);
+    }
+    else if (e.target.textContent == "=")   {   // si le bouton cliqué est '='
+        SetOperation();
+    }
 }
 
 // fontion pour clear l'écran
-function clearScreen(e)  {
-
-    if (e.target.textContent == clearButton.textContent)    {
-        screen.textContent = "0";
-        lastButtonPressed = "C";
-    }
+function clearScreen()  {
+    screen.textContent = "0";
+    lastButtonPressed = "C";    
 }
 
 // fonction pour ajouter un chiffre
-function addNumber(e)    {
-    
-    if (chiffres.includes(e.target.textContent))    {   // si le bouton cliqué est un chiffre
-        if (operatorSelected != "") {   // si l'utilisateur a cliqué sur un opérateur ("+", "-", "x", "/") avant de cliquer sur ce bouton
-            screen.textContent = e.target.textContent;  // on remplace le nombre actuellement affiché sur l'écran par le nouveau chiffre choisi
-            operatorSelected = ""; 
-        }
-        else    {                               // SINON
-            if (screen.textContent == "0")  {   // si l'écran affiche "0", on le remplace par le nouveau chiffre
-                screen.textContent = e.target.textContent;
-            }
-            else    {                           // sinon on ajoute le nouveau chiffre au chiffre déjà affiché sur l'écran
-                screen.textContent += e.target.textContent;
-            }
-        }        
+function addNumber(e)    { 
+    if (operatorSelected != "") {   // si l'utilisateur a cliqué sur un opérateur ("+", "-", "x", "/") avant de cliquer sur ce bouton
+        screen.textContent = e.target.textContent;  // on remplace le nombre actuellement affiché sur l'écran par le nouveau chiffre choisi
+        operatorSelected = ""; 
     }
+    else    {                               // SINON
+        if (screen.textContent == "0")  {   // si l'écran affiche "0", on le remplace par le nouveau chiffre
+            screen.textContent = e.target.textContent;
+        }
+        else    {                           // sinon on ajoute le nouveau chiffre au chiffre déjà affiché sur l'écran
+            screen.textContent += e.target.textContent;
+        }
+    }        
 }
 
 // fonction qui lance l'éxécution de l'opération lorsque l'utilisateur clique sur "="
-function SetOperation(e) {
-
-    if (e.target.textContent == "=")    {
+function SetOperation() {
+    if (lastButtonPressed != "=")   {
         secondNumber = screen.textContent;
-        operate(currentOperator, Number(firstNumber), Number(screen.textContent));
+        operate(currentOperator, Number(firstNumber), Number(screen.textContent));    
+        lastButtonPressed = "=";
+    }
+    else    {
+        console.log(operatorSelected);
+        // - 100 5  95 5
+        if (currentOperator == "-" || currentOperator == "/") {
+            operate(currentOperator, Number(screen.textContent), Number(secondNumber));   
+            console.log("yoooo");
+        }
+        else    {
+            firstNumber = secondNumber;
+            operate(currentOperator, Number(firstNumber), Number(screen.textContent));    
+        }
     }
 }
 
 
 // fonction qui stocke dans une variable quel opérateur a été choisi
 function setOperator(e)  {
-    if (operators.includes(e.target.textContent))   {   
-        operatorSelected = e.target.textContent;    
-        currentOperator = e.target.textContent;
-        firstNumber = screen.textContent;
-        console.log("first number : " + firstNumber);
-    }
+    operatorSelected = e.target.textContent;    
+    currentOperator = e.target.textContent;
+    firstNumber = screen.textContent;
+    console.log("first number : " + firstNumber);
+
 }
 
 
@@ -78,15 +91,17 @@ function sum(a, b)  {
 }
 
 function subtract(a, b) {
-    return a - b;
+    console.log("premier nombre = " + a);
+    console.log("second nombre = " + b);
+    screen.textContent = a - b;
 }
 
 function multiply (a, b)    {
-    return a * b;
+    screen.textContent = a * b;
 }
 
 function divide(a, b)   {
-    return a / b;
+    screen.textContent = a / b;
 }
 
 // fonction qui fait l'opération mathématique avec l'opérateur et les deux nombres en arguments
@@ -97,7 +112,7 @@ function operate(operator, a, b)    {
             return sum(a, b);
         case "-":
             return subtract(a, b);
-        case "*":
+        case "x":
             return multiply(a, b);
         case "/":
             return divide(a, b);
